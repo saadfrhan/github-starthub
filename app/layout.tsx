922160c8b0adf03fb09327dcdfa7ddaed1eb0883
saadfrhan <saadfarhan347@gmail.com>
2024-05-08 08:17:08 +0500

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { HydrationOverlay } from "@builder.io/react-hydration-overlay";
 import { Toaster } from "@/components/ui/sonner";
 
 const segoeUI = localFont({
@@ -10,8 +9,32 @@ const segoeUI = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Explore Repositories",
-  description: "Explore repositories from GitHub",
+  metadataBase: new URL("https://github-starthub.vercel.app"),
+  title: "GitHub StartHub",
+  description: "Explore your GitHub profile in a beautiful way",
+  openGraph: {
+    title: "GitHub StartHub",
+    description: "Explore your GitHub profile in a beautiful way",
+    url: "https://github-starthub.vercel.app",
+    siteName: "GitHub StartHub",
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "GitHub StartHub",
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
@@ -21,10 +44,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <HydrationOverlay>
-        <body className={`${segoeUI.className} dark`}>{children}</body>
-        <Toaster />
-      </HydrationOverlay>
+      <body className={`${segoeUI.className} dark`}>{children}</body>
+      <Toaster />
     </html>
   );
 }
